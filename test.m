a12647cd33caae04006a2b1b42d09035eeb3dede
Robert Bittner <robert.bittner@gmx.de>
2016-04-21 15:30:33 +0200

    This is a test

---
+++ test.m
@@ -0,0 +1,25 @@
+function test();
+
+
+    %cd '/Users/mmb/MEG/ATWM1/Presentation';
+
+    cd '/data/projects/ATWM1/Presentation';    
+    
+    ! git status
+    
+    % get changes from GitHub
+    ! git pull
+    
+    % add new files and directories
+    eval(sprintf('! git add /data/projects/ATWM1/Presentation/test.m'))
+    
+    % commit changes
+    eval(sprintf('! git commit -m "This is a test"'));
+    
+    % push to GitHub
+    ! git push
+    
+    ! git status
+
+
+end
