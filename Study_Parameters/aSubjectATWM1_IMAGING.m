a71f1aa90f52bd9b8f05c257418d64d700e89106
Robert Bittner <robert.bittner@gmx.de>
2017-12-08 14:52:39 +0100

    Add new presentation scenario files

--- Study_Parameters/aSubjectATWM1_IMAGING.m
+++ Study_Parameters/aSubjectATWM1_IMAGING.m
@@ -121,6 +121,7 @@ aSubject.ATWM1_IMAGING.Groups.SCHI	= {
 aSubject.ATWM1_IMAGING.Groups.SCHI = sort(aSubject.ATWM1_IMAGING.Groups.SCHI);
 
 aSubject.ATWM1_IMAGING.Groups.BIPO	= {
+	'ED96FHG'
 	};
 aSubject.ATWM1_IMAGING.Groups.BIPO = sort(aSubject.ATWM1_IMAGING.Groups.BIPO);
 
